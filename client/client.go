96f46b3f228476d36290acef6fec429a88797b3b
QuangNV23062004 <nguyenquang23062004@gmail.com>
2026-01-03 22:52:50 +0700

    grpc example

---
+++ client/client.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"time"
+
+	pb "github.com/QuangNV23062004/grpc-example/coffeeshop_proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost:9001", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Failed to connect to gprc server, %s", err)
+	}
+
+	defer conn.Close()
+
+	c := pb.NewCoffeeShopServiceClient(conn)
+	log.Println("Connected to gRPC server successfully")
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	menuStream, err := c.GetMenu(ctx, &pb.MenuRequest{})
+	if err != nil {
+		log.Fatalf("Error while calling GetMenu: %s", err)
+	}
+
+	done := make(chan bool)
+
+	var items []*pb.Item
+
+	go func() {
+		for {
+			menuResp, err := menuStream.Recv()
+			if err == io.EOF {
+				done <- true
+				return
+			}
+			if err != nil {
+				log.Printf("Error while receiving menu item: %s", err)
+				close(done)
+				return
+			}
+			items = menuResp.Items
+			log.Println("Received menu items:")
+			for _, item := range items {
+				log.Printf("Item ID: %s, Name: %s", item.GetId(), item.GetName())
+			}
+		}
+	}()
+
+	<-done
+
+	receipt, err := c.PlaceOrder(ctx, &pb.OrderRequest{Items: items})
+	if err != nil {
+		log.Fatalf("Error while placing order: %s", err)
+	}
+
+	log.Printf("Order placed successfully, receipt ID: %s", receipt.GetId())
+
+	orderStatus, err := c.GetOrderStatus(ctx, receipt)
+	if err != nil {
+		log.Fatalf("Error while getting order status: %s", err)
+	}
+	log.Printf("Order status for receipt ID %s: %s", orderStatus.GetOrderId(), orderStatus.GetStatus())
+}
---
+++ server.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/QuangNV23062004/grpc-example/coffeeshop_proto"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedCoffeeShopServiceServer
+}
+
+func (s *server) GetMenu(req *pb.MenuRequest, res pb.CoffeeShopService_GetMenuServer) error {
+	items := []*pb.Item{
+		{Id: "1", Name: "Espresso"},
+		{Id: "2", Name: "Cappuccino"},
+		{Id: "3", Name: "Latte"},
+	}
+
+	log.Println("Sending menu items to client...")
+
+	for i, _ := range items {
+		res.Send(&pb.MenuResponse{Items: items[0 : i+1]})
+	}
+
+	return nil
+}
+
+func (s *server) PlaceOrder(ctx context.Context, order *pb.OrderRequest) (*pb.Receipt, error) {
+	return &pb.Receipt{Id: "12345"}, nil
+}
+
+func (s *server) GetOrderStatus(ctx context.Context, receipt *pb.Receipt) (*pb.OrderStatus, error) {
+	return &pb.OrderStatus{OrderId: receipt.Id, Status: "In Progress"}, nil
+}
+
+func main() {
+	//setup listener on 9001
+	listener, err := net.Listen("tcp", ":9001")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9001: %v", err)
+	}
+
+	fmt.Println("gRPC server listening on port 9001")
+	grpcServer := grpc.NewServer()
+	pb.RegisterCoffeeShopServiceServer(grpcServer, &server{})
+
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve gRPC server: %v", err)
+	}
+
+}
